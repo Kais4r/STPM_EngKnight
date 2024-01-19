854486c4982bed4fc2f22c49a96be0c682d428ab
Kais4r <143178887+Kais4r@users.noreply.github.com>
2024-01-19 18:27:36 +0700

    Update battle scene

---
+++ Assets/GameAssets/Scenes/Battle/Scripts/UI/BattleUIMoveHorizontal.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BattleUIMoveHorizontal : MonoBehaviour
+{
+    [SerializeField] private GameObject _userInterfaceObject;
+    private RectTransform _rectTransform;
+    [SerializeField] private int originalX, targetX;
+    private int state = 0;
+    private Vector2 newPos;
+
+
+    private void Start()
+    {
+        _rectTransform = _userInterfaceObject.GetComponent<RectTransform>();
+        newPos = _rectTransform.anchoredPosition;
+    }
+
+    public void MoveUIObjectHorizontal()
+    {
+        if (state == 0)
+        {
+            state = 1;
+            newPos.x = targetX;
+        }
+        else
+        {
+            state = 0;
+            newPos.x = originalX;
+        }
+        _rectTransform.anchoredPosition = newPos;
+    }
+}
