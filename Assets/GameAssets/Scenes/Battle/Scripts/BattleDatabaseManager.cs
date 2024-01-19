95c5fb146de6baac7704ad21f10cf2976775ba2d
Kais4r <143178887+Kais4r@users.noreply.github.com>
2024-01-19 16:28:58 +0700

    Battle scene ui layout

---
+++ Assets/GameAssets/Scenes/Battle/Scripts/BattleDatabaseManager.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BattleDatabaseManager : MonoBehaviour
+{
+    [SerializeField] private string databaseName;
+    private string streamingAssetdataPath;
+    private string jsonString;
+    List<EnglishWord> danhSachTuVung;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        streamingAssetdataPath = Application.streamingAssetsPath + "/" + databaseName + ".json";
+        LoadData();
+    }
+
+    private void LoadData()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            StartCoroutine(LoadDataAndroid());
+        }
+        else
+        {
+            jsonString = File.ReadAllText(streamingAssetdataPath);
+            ProcessData();
+        }
+    }
+
+    private IEnumerator LoadDataAndroid()
+    {
+        // Create a UnityWebRequest to load the file
+        UnityWebRequest www = UnityWebRequest.Get(streamingAssetdataPath);
+
+        // Send the request and wait for it to finish
+        yield return www.SendWebRequest();
+
+        // Check for any errors during the request
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            // Get the downloaded text
+            jsonString = www.downloadHandler.text;
+            ProcessData();
+        }
+    }
+
+    private void ProcessData()
+    {
+        danhSachTuVung = JsonConvert.DeserializeObject<List<EnglishWord>>(jsonString);
+        //EnglishWord word = danhSachTuVung[1];
+
+        Debug.Log(danhSachTuVung);
+    }
+}
